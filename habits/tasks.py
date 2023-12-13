66ff5700be9e36186800af6df85cec6011039e29
andre059 <andreyperm159@mail.ru>
2023-12-13 23:08:24 +0500

    Бот

--- habits/tasks.py
+++ habits/tasks.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from django.core.management import call_command
 
 from habits.models import Habit
-from users.management.commands.testn import Command
 
 
 @shared_task
@@ -18,20 +17,3 @@ def schedule_reminder(habit_id):
             habit.sign_pleasant_habit = True
             habit.save()
             call_command('command')
-
-    # habitude = Habit.objects.filter(sign_pleasant_habit=False, time__lte=datetime.now() - timedelta(days=7))
-    # for habit in habitude:
-    #     if (datetime.now() - habit.time).days > 7:
-    #         habit.sign_pleasant_habit = True
-    #         habit.save()
-    #         call_command('command')
-
-
-            # Command.call_command('apply_async')
-
-    # Command.apply_async(countdown=7*24*60*60)
-
-    # habitude = Habit.objects.all()
-    # for habit in habitude:
-    #     created_at = habit.time
-    #     if not habit.sign_pleasant_habit and created_at + timedelta(days=7) < datetime.now():
