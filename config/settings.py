ffb0d5e7aab06faab5125ef26d7cf32dae8b665c
andre059 <andreyperm159@mail.ru>
2023-12-22 16:16:19 +0500

    настройка и запуск Docker Compose

--- config/settings.py
+++ config/settings.py
@@ -91,9 +91,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASES_NAME'),
-        'USER': os.getenv('DATABASES_USER'),
-        'PASSWORD': os.getenv('DATABASES_PASSWORD'),
+        'NAME': 'postgres',  # os.getenv('DATABASES_NAME'),
+        'USER': 'postgres',  # os.getenv('DATABASES_USER'),
+        'PASSWORD': 'mysecretpassword',  # os.getenv('DATABASES_PASSWORD'),
+        'HOST': 'db'
     }
 }
 
