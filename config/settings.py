a2f0be95ce43efb8a9c300f2a5966752f262dd89
andre059 <andreyperm159@mail.ru>
2023-12-23 22:21:20 +0500

    настройка docker-compose celery

--- config/settings.py
+++ config/settings.py
@@ -185,7 +185,7 @@ SWAGGER_SETTINGS = {
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_TIMEZONE = "Europe/Moscow"
 CELERY_TASK_TRACK_STARTED = True
