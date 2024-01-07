f78c97cde2e2941c4b6e1b5f3a69a53f72232c4f
andre059 <andreyperm159@mail.ru>
2024-01-07 20:13:59 +0500

    settings fix

--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+ENV_TYPE = os.getenv('ENV_TYPE')
+
+if ENV_TYPE == 'local':
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static',
+    )
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
